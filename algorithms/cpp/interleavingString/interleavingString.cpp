36cf54ca9494d953abf4a73e3acb7444b5911960
410588896 <bazingayu@tencent.com>
2020-12-18 09:34:27 +0800

    feat:add interleavingString

---
+++ algorithms/cpp/interleavingString/interleavingString.cpp
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode id=97 lang=cpp
+ *
+ * [97] Interleaving String
+ */
+
+/***************************************************
+
+Given strings s1, s2, and s3, find whether s3 is formed by an interleaving of s1 and s2.
+
+An interleaving of two strings s and t is a configuration where they are divided into non-empty substrings such that:
+
+s = s1 + s2 + ... + sn
+t = t1 + t2 + ... + tm
+|n - m| <= 1
+The interleaving is s1 + t1 + s2 + t2 + s3 + t3 + ... or t1 + s1 + t2 + s2 + t3 + s3 + ...
+Note: a + b is the concatenation of strings a and b.
+
+ 
+
+Example 1:
+
+
+Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+Output: true
+Example 2:
+
+Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+Output: false
+Example 3:
+
+Input: s1 = "", s2 = "", s3 = ""
+Output: true
+ 
+
+Constraints:
+
+0 <= s1.length, s2.length <= 100
+0 <= s3.length <= 200
+s1, s2, and s3 consist of lower-case English letters.
+
+***************************************************/
+
+// @lc code=start
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        int n = s1.size(), m = s2.size(), t = s3.size();
+        vector<int> f(m + 1, false);
+        if (n + m != t) return false;
+        f[0] = true;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                int p = i + j - 1;
+                if (i > 0) f[j] &= (s3[p] == s1[i - 1]);
+                if (j > 0) f[j] |= (f[j - 1] && s3[p] == s2[j - 1]);
+            }
+        }
+        return f[m];
+    }
+};
+// @lc code=end
+
+
