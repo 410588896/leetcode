6434a1b7b4688d94b7627ce4a2132c60182ff41d
410588896 <410588896@qq.com>
2021-01-17 21:59:07 +0800

    feat:add sort list

--- algorithms/cpp/sortList/sortList.cpp
+++ algorithms/cpp/sortList/sortList.cpp
@@ -5,34 +5,34 @@
  */
 
 /************************************
-* 
+*
 * Given the head of a linked list, return the list after sorting it in ascending order.
-* 
+*
 * Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
-* 
-*  
-* 
+*
+*
+*
 * Example 1:
-* 
-* 
+*
+*
 * Input: head = [4,2,1,3]
 * Output: [1,2,3,4]
 * Example 2:
-* 
-* 
+*
+*
 * Input: head = [-1,5,3,4,0]
 * Output: [-1,0,3,4,5]
 * Example 3:
-* 
+*
 * Input: head = []
 * Output: []
-*  
-* 
+*
+*
 * Constraints:
-* 
+*
 * The number of nodes in the list is in the range [0, 5 * 104].
 * -105 <= Node.val <= 105
-* 
+*
 ************************************/
 
 // @lc code=start
@@ -79,7 +79,7 @@ public:
             length++;
         }
         ListNode *dummyHead = new ListNode(-1, head);
-        for (int i = 0; i < length; i <<= 1) {
+        for (int i = 1; i < length; i <<= 1) {
             ListNode *prev = dummyHead, *cur = dummyHead->next;
             while (cur != nullptr) {
                 ListNode *head1 = cur;
@@ -89,13 +89,27 @@ public:
                 ListNode *head2 = cur->next;
                 cur->next = nullptr;
                 cur = head2;
-                for (int j = 0; j < i && cur != nullptr && cur->next != nullptr; i++) {
+                for (int j = 1; j < i && cur != nullptr && cur->next != nullptr; j++) {
                     cur = cur->next;
                 }
+                ListNode *next = nullptr;
+                if (cur != nullptr) {
+                    next = cur->next;
+                    cur->next = nullptr;
+                }
+                ListNode *merged = merge(head1, head2);
+                prev->next = merged;
+                while (prev->next != nullptr) {
+                    prev = prev->next;
+                }
+                cur = next;
             }
         }
+        return dummyHead->next;
     }
 };
 // @lc code=end
+
+
 
 
