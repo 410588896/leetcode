004ca369000c4b7bf4a900dd9f78c42f39b2a6db
410588896 <410588896@qq.com>
2022-03-06 14:25:53 +0800

    add addDigits

---
+++ algorithms/cpp/addDigits/addDigits.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=258 lang=cpp
+ *
+ * [258] 各位相加
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int addDigits(int num) {
+        while (num >= 10) {
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+};
