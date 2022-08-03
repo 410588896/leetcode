1a24d06495cf4a43717f2c24d3396512c901a6b9
410588896 <410588896@qq.com>
2022-08-03 23:48:55 +0800

    add orderlyQueue

---
+++ algorithms/cpp/orderlyQueue/orderlyQueue.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=899 lang=cpp
+ *
+ * [899] 有序队列
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string orderlyQueue(string s, int k) {
+        if (k == 1) {
+            string smallest = s;
+            int n = s.size();
+            for (int i = 0; i < n; i++) {
+                char c = s[0];
+                s = s.substr(1);
+                s.push_back(c);
+                if (s < smallest) {
+                    smallest = s;
+                }
+            }
+            return smallest;
+        } else {
+            sort(s.begin(), s.end());
+            return s;
+        }
+    }
+};
+// @lc code=end
+
+
