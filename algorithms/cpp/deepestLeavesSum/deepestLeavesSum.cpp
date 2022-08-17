240b674bc348a285063abc6cbd6546ee1ffa4ef6
410588896 <410588896@qq.com>
2022-08-17 23:40:18 +0800

    add deepestLeavesSum

---
+++ algorithms/cpp/deepestLeavesSum/deepestLeavesSum.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=1302 lang=cpp
+ *
+ * [1302] 层数最深叶子节点的和
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int deepestLeavesSum(TreeNode* root) {
+        int sum = 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()) {
+            sum = 0;
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode *node = q.front();
+                q.pop();
+                sum += node->val;
+                if (node->left != nullptr) {
+                    q.push(node->left);
+                }
+                if (node->right != nullptr) {
+                    q.push(node->right);
+                }
+            }
+        }
+        return sum;
+    }
+};
+// @lc code=end
+
+
