ccd5d7df15ac52a7fa1263ce674e7324043ee996
410588896 <410588896@qq.com>
2022-08-29 23:43:35 +0800

    add lowestCommonAncestorOfABinaryTree_digui.cpp

---
+++ algorithms/cpp/lowestCommonAncestorOfABinaryTree/lowestCommonAncestorOfABinaryTree_digui.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=236 lang=cpp
+ *
+ * [236] 二叉树的最近公共祖先
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    TreeNode* res;
+    bool dfs(TreeNode *root, TreeNode *p, TreeNode *q) {
+        if (root == nullptr) return false;
+        bool lson = dfs(root->left, p, q);
+        bool rson = dfs(root->right, p, q);
+        if ((lson && rson) || ((lson || rson) && (root->val == p->val || root->val == q->val))) {
+            res = root;
+        }
+        return lson || rson || (root->val == p->val || root->val == q->val);
+    }
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        dfs(root, p, q);
+        return res;
+    }
+};
+// @lc code=end
+
+
