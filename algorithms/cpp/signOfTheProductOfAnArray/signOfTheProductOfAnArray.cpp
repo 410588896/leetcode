4a61d5272854198b64a63b2c8362e363e80b5ab1
410588896 <410588896@qq.com>
2022-10-27 10:18:29 +0800

    add signOfTheProductOfAnArray

---
+++ algorithms/cpp/signOfTheProductOfAnArray/signOfTheProductOfAnArray.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=1822 lang=cpp
+ *
+ * [1822] 数组元素积的符号
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int sign = 1;
+        for (auto num : nums) {
+            if (num == 0) return 0;
+            if (num < 0) {
+                sign = -sign;
+            }
+        }
+        return sign;
+    }
+};
+// @lc code=end
+
+
